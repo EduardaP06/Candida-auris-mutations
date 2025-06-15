f0f72e477085c9b75f770982593a369755c181be
Eduarda Pérez <edu.sofia4@gmail.com>
2025-06-14 20:28:18 -0500

    Delete run_blastx.sh

--- run_blastx.sh
+++
@@ -1,2 +0,0 @@
-blastx -query AQ20.fasta -db genes_wt_proteins.fasta -out blastx_results/candida01_blastx.tsv -outfmt 6
-blastx -query AQ30.fasta -db genes_wt_proteins.fasta -out blastx_results/auris01_blastx.tsv -outfmt 6
